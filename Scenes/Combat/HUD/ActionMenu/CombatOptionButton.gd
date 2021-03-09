3bb35daae927c16b173fe9fd46226fd79c355649
Hugo Baba <Hugobarette@gmail.com>
2021-03-09 14:39:51 +0100

    Fix little flickering in menu option apperance

--- Scenes/Combat/HUD/ActionMenu/CombatOptionButton.gd
+++ Scenes/Combat/HUD/ActionMenu/CombatOptionButton.gd
@@ -2,45 +2,54 @@ extends MenuOptionsBase
 class_name CombatOptionButton
 
 onready var tween = $Tween
+onready var default_margin_left = get_margin(MARGIN_LEFT)
 
+export var transition_duration : float = 0.2
+export var hidden : bool = true setget set_hidden, is_hidden
+export var hidden_margin_left : float = 30.0
 
 #### ACCESSORS ####
 
-func set_visible(value: bool):
-	if value != visible:
-		visible = value
-		if is_ready:
-			if visible:
-				appear()
-			else:
-				disappear()
+func set_hidden(value: bool): hidden = value
+func is_hidden() -> bool: return hidden
 
 
 #### BUILT-IN ####
 
 
+func _ready() -> void:
+	if is_hidden():
+		set_margin(MARGIN_LEFT, hidden_margin_left)
+		set_modulate(Color.transparent)
 
 
 #### LOGIC ####
 
 func appear():
 	tween.interpolate_property(self, "modulate",
-		Color.transparent, Color.white, 0.3,
+		Color.transparent, Color.white, transition_duration,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	
 	tween.interpolate_property(self, "margin_left",
-		30, 0, 0.3,
+		hidden_margin_left, default_margin_left, transition_duration,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	
 	tween.start()
+	
+	yield(tween, "tween_all_completed")
+	set_hidden(false)
+
 
 func disappear():
 	tween.interpolate_property(self, "modulate",
-		Color.white, Color.transparent,0.3,
+		Color.white, Color.transparent, transition_duration,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	
 	tween.interpolate_property(self, "margin_left",
-		0, 30, 0.3,
+		default_margin_left, hidden_margin_left, transition_duration,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	
 	tween.start()
+	
+	yield(tween, "tween_all_completed")
+	set_hidden(true)
